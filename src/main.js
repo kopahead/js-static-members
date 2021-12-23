c67bd0b6c03371bce063fe6465056bd82fbed620
Elvis <kopahead@gmail.com>
2021-12-23 16:44:08 +0200

    Add static tests

--- src/main.js
+++ src/main.js
@@ -2,10 +2,39 @@
 class Dog {
     static breed = "German Shepherd";
     static speed() {
-      return "48 km"
+      return "48 km/h"
+    }
+}
+
+class Cat {
+    constructor (breed) {
+        this.breed = breed;
+
+        this.breedDictionary = {
+            "bengal": {
+                "name": "Bengal",
+                "speed": "56.3 km/h"
+            }
+        }
+    }
+
+    info() {
+        const breed = this.breedDictionary[this.breed];
+        return `The ${breed.name}, travels a speed of ${breed.speed}`;
+    }
+
+    static staticClassThatThrowsError() {
+        const breed = this.breedDictionary[this.breed];
+        return `The ${breed.name}, travels a speed of ${breed.speed}`;
+    }
+
+    static staticClassThatUsesAClassInstance(breed) {
+        const cat = new Cat(breed);
+        return cat.info();
     }
 }
 
 module.exports = {
-    Dog
+    Dog,
+    Cat
 };
--- src/main.test.js
+++ src/main.test.js
@@ -1,12 +1,45 @@
-const { Dog } = require('./main');
+const { Dog, Cat } = require('./main');
+
 
 test('Tests that the dog is a German Shepherd (Static)', () => {
   let breed = Dog.breed;
-  expect(breed).toEqual("German Shepherd");
+  expect(breed).toEqual("German Shepherd"); // ✅ Pass
 });
+
+test('Tests that the dog has a speed of 48 km/h (Static)', () => {
+  let breed = Dog.speed();
+  expect(breed).toEqual("48 km/h"); // ✅ Pass
+});
+
 
 test('Tests that the dog is a German Shepherd (Instantiated)', () => {
   const dog = new Dog();
   const breed = dog.breed;
-  expect(breed).toEqual("German Shepherd");
+  expect(breed).toBeUndefined(); // ✅ Pass
+});
+
+test('Tests that the dog has a speed of 48 km/h (Instantiated)', () => {
+  const dog = new Dog();
+
+  expect(() => {
+    dog.speed();
+  }).toThrow();  // ✅ Pass
+});
+
+test('Tests that the cat information can be accessed (Instantiated)', () => {
+  const cat = new Cat('bengal');
+
+  expect(cat.info()).toEqual('The Bengal, travels a speed of 56.3 km/h'); // ✅ Pass
+});
+
+test('Tests that the cat information can be accessed (Static)', () => {
+  expect(() => {
+    const cat = Cat('bengal');
+    cat.staticClassThatThrowsError();
+  }).toThrow();  // ✅ Pass // Class constructor Cat cannot be invoked without 'new'
+});
+
+test('Tests that the cat information can be accessed (Static)', () => {
+  const cat = Cat;
+  expect(cat.staticClassThatUsesAClassInstance('bengal')).toEqual('The Bengal, travels a speed of 56.3 km/h');  // ✅ Pass
 });
